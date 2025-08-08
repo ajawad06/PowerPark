e791491ae7e2f2be0a438cfee67019cf56d059ed
Abdullah Jawad <abdullahjawad.2005@gmail.com>
2025-08-08 21:23:01 +0500

    data added

---
+++ database/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO charging_station (station_name, location, longitude, charger_type, total_slots, available_slots)
+VALUES ('EVX', 'F8-Islamabad','33.71016', '73.051938', 'Fast', 4, 4),
+VALUES ('PSO Charging Station', 'F7-Islamabad','33.722763', '73.061038', 'Standard', 2, 2),
+VALUES ('Go Green Avenue', 'I9-Islamabad','33.663120', '73.057170', 'Fast', 4, 4);
+
+INSERT INTO admin(username,password,email,phone_num)
+VALUES ('Admin1','admin123','admin1@gmail.com','03336567899'),
+VALUES ('Admin2','admin789','admin2@gmail.com','03336467899');
+
+INSERT INTO user(username,password,email,phone_num,address)
+VALUES ('Abdullah','abd@1234','abdullah@gmail.com','03333636363','Islamabad');
